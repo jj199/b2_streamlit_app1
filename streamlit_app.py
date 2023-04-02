a119c8371abfddda00cebd7287c024d8857568fb
jj199 <46914176+jj199@users.noreply.github.com>
2023-04-02 22:00:36 +1000

    Update streamlit_app.py
    adding code to display fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,8 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
- 
   
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 
 import pandas
 my_fruit_list =  pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -24,4 +22,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
+# new section to display fruityvice api response 
+streamlit.header("Fruityvice Fruit Advice!")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
